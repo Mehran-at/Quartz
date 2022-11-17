dc8da6720ed2fece8b7d5758afb3536836336a3e
Mehran Hosseini <mehran.hosseini@ebcont.com>
2022-11-17 16:24:39 +0100

    Add log

--- src/main/java/com/kingcode/quartz/simpleService/job/SampleJob.java
+++ src/main/java/com/kingcode/quartz/simpleService/job/SampleJob.java
@@ -2,12 +2,13 @@ package com.kingcode.quartz.simpleService.job;
 
 import com.kingcode.quartz.simpleService.service.SimpleService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.stereotype.Component;
-import org.quartz.Job;
 
-
+@Slf4j
 @Component
 @AllArgsConstructor
 public class SampleJob implements Job {
@@ -16,6 +17,12 @@ public class SampleJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        log.info(
+            "Job ** {} ** fired @ {}",
+            jobExecutionContext.getJobDetail().getKey().getName(),
+            jobExecutionContext.getFireTime()
+        );
         simpleService.simpleMethod();
+        log.info("Next job scheduled @ {}", jobExecutionContext.getNextFireTime());
     }
 }
